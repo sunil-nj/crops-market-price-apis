0cd23cd013af4f6ddf15cd463452e802dcc62fca
sunil666 <30348420+sunil666@users.noreply.github.com>
2021-09-18 12:35:55 +0530

    first commit

---
+++ src/main/java/com/app/cropsMarketPrice/CropsMarketPriceApplication.java
@@ -0,0 +1,13 @@
+package com.app.cropsMarketPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsMarketPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropsMarketPriceApplication.class, args);
+	}
+
+}
